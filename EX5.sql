0e6d3e7b863d3d164f2a4a007f14a48cde8782e9
Loni Tande <ebenyemiriam17@gmail.com>
2023-04-03 21:20:48 +0200

    Delete EX5.sql

--- EX5.sql
+++
@@ -1,8 +0,0 @@
--- Get all Bands that have No Albums 
--- Return the band name as 'Band Name'
-
-SELECT b.name AS 'Band Name'
-FROM bands AS b
-LEFT JOIN albums AS a 
-ON b.id = a.band_id 
-WHERE a.name IS NULL;
